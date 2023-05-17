010eb3a75aa58c2eb5617473266b9150e727f008
marvin <marvin@finfiles.nl>
2023-05-17 11:14:04 +0800

    Using JSON to seed

--- laraveldb/README.md
+++ laraveldb/README.md
@@ -1,29 +1,64 @@
-## Squash migrations
-### Result is the file generated is located to ./database/schema
-php artisan schema:dump
+# Mastering Migrations
 
+### Squashing migrations
+### Result is the file generated is located to ./database/schema
+````
+php artisan schema:dump
+````
 
 ### Pruning migration
-php artisan migrate:reset <br/>
-then <br/>
-php artisan schema:dump --prune
+````
+php artisan migrate:reset
 
+then
+
+php artisan schema:dump --prune
+````
 
 ### Adding columns, Renaming columns, Delete column
+````
 php artisan make:migration verb_col_in_tbl --table=tbl_you_want_to_renames
+````
 
 ### Factories and Seeders
 Seeders - real is needed.<br/>
 Factories - used when generating fake data.
 
-#### Creating Factories
-php artisan make:factory PostFactory <br/>
+### Creating Factories
+````
+php artisan make:factory PostFactory
 php artisan make:model TableName -f (-f flag would generate a factory)
+````
 
+### Creating Seeders 
+````
+php artisan make:seeder PostSeeder
 
-#### Creating Seeders 
-php artisan make:seeder PostSeeder <br/>
-php artisan db:seed (only works when you add the factory inside the factory seeder.) <br/>
+(only works when you add the factory inside the factory seeder.)
+php artisan db:seed
+ 
 or  php artisan db:seed --class=NameOfSeeder
-<br/>
+
 php artisan migrate --seed will migrate first all migrations then seed
+````
+### Another best practice of using Seeder
+Create a json file instead then use that to populate DB <br/>
+create database/json/table.json to which we are going to seed <br/>
+Then, <br/>
+````
+$json = File::get('database/json/posts.json'); <br/>
+$posts = collect(json_decode($json)); <br/>
+````
+Then loop using ->each()
+````
+$posts->each(function ($post) {
+    Post::create([
+    'title' => $post->title,
+    'slug' => $post->slug,
+    'excerpt' => $post->excerpt,
+    'content' => $post->content,
+    'is_published' => $post->is_published,
+    'min_to_read' => $post->min_to_read
+    ]);
+});
+````
--- laraveldb/database/seeders/PostSeeder.php
+++ laraveldb/database/seeders/PostSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostSeeder extends Seeder
 {
@@ -13,41 +13,25 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //Make a collection of data to insert
-        $posts = collect([
-            [
-                'title' => 'Post One',
-                'slug' => 'post-one',
-                'excerpt' => 'Excerpt of post one',
-                'content' => 'Content of post one',
-                'is_published' => true,
-                'min_to_read' => 2
-            ],
-            [
-                'title' => 'Post Two',
-                'slug' => 'post-two',
-                'excerpt' => 'Excerpt of post two',
-                'content' => 'Content of post two',
-                'is_published' => true,
-                'min_to_read' => 2
-            ],
-        ]);
+        //From hard coded json here, we will use the json we created.
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
         /**
          * We could use foreach loop but since its a collection
          * we could use ->each() looping all over posts
          */
         $posts->each(function ($post) {
-            //Downside of insert, it won't add timestamp for created_at updated_at
-//            Post::insert($post);
-            //Instead use
+            // Downside of insert, it won't add timestamp for created_at updated_at
+            // Post::insert($post);
+            // Instead use
             Post::create([
-                'title' => $post['title'],
-                'slug' => $post['slug'],
-                'excerpt' => $post['excerpt'],
-                'content' => $post['content'],
-                'is_published' => $post['is_published'],
-                'min_to_read' => $post['min_to_read']
+                'title' => $post->title,
+                'slug' => $post->slug,
+                'excerpt' => $post->excerpt,
+                'content' => $post->content,
+                'is_published' => $post->is_published,
+                'min_to_read' => $post->min_to_read
             ]);
         });
     }
